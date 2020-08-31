06402affe5b3d65a82b9fb7c00e9281a6dc5a6e3
fahadkhaanz <fk0881889@gmail.com>
2020-08-31 12:43:25 +0530

    MIxture spoj

---
+++ Mixtures.cpp
@@ -0,0 +1,136 @@
+//git pull --rebase origin master
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, -1, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define wi(t) int t;cin>>t;while(t--)
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+int mpow(int base, int exp); 
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1000000007;
+const int N = 3e5, M = N;
+//=======================
+
+vi g[N];
+int a[N];
+ll int gcd(ll int a, ll int b) 
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b);  
+      
+} 
+int tc=1;
+ll int dp[1001][1001];
+
+vi ar(1000);
+int sum(int i,int j)
+{   int s=0;
+    for(int k=i;k<=j;k++)
+    {s+=ar[k];s=s%100;}
+    return s;
+}
+ll int mcm(int i,int j)
+{
+        if(i>=j)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        dp[i][j]=INT_MAX;
+        for(int k=i;k<=j;k++)
+        {   
+            dp[i][j]=min(dp[i][j],mcm(i,k)+mcm(k+1,j)+sum(i,k)*sum(k+1,j));
+        }
+        return dp[i][j];
+}
+void solve()
+    { 
+        int n;
+        while(cin>>n){
+        fo(i,n) cin>>ar[i];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++)
+            dp[i][j]=-1;
+        }
+        cout<<mcm(0,n-1)<<"\n";
+        }
+    }
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+   // wi(t)
+    {
+      solve();
+    }
+
+    return 0;
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+void ipgraph(int n, int m){
+    int i, u, v;
+    while(m--){
+        cin>>u>>v;
+    u--, v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+}
+
+void dfs(int u, int par){
+    for(int v:g[u]){
+        if (v == par) continue;
+        dfs(v, u);
+    }
+}
+
+
+
+
+
