0e38f422174ae0d4b2461d50a6e4b1c3f9deac30
fahadkhaanz <fk0881889@gmail.com>
2020-09-25 12:33:43 +0530

    Codeforces Round #672 (Div. 2)

---
+++ A_Cubes_Sorting.cpp
@@ -0,0 +1,194 @@
+//git pull --rebase origin master
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define wi(t) int t;cin>>t;while(t--)
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+int mpow(int base, int exp); 
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1000000007;
+const int N = 3e5, M = N;
+//=======================
+
+vi g[N];
+int a[N];
+ll int gcd(ll int a, ll int b) 
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b);  
+      
+} 
+ll int merge(ll int arr[], ll int temp[], int left, int mid, int right) 
+{ 
+	ll int inv_count = 0; 
+
+	int i = left; /* i is index for left subarray*/
+	int j = mid; /* i is index for right subarray*/
+	int k = left; /* i is index for resultant merged subarray*/
+	while ((i <= mid - 1) && (j <= right)) 
+	{ 
+		if (arr[i] <= arr[j]) 
+			temp[k++] = arr[i++]; 
+		else
+		{ 
+			temp[k++] = arr[j++]; 
+
+			/* this is tricky -- see above explanation/ 
+			diagram for merge()*/
+			inv_count = inv_count + (mid - i); 
+		} 
+	} 
+
+	/* Copy the remaining elements of left subarray 
+	(if there are any) to temp*/
+	while (i <= mid - 1) 
+		temp[k++] = arr[i++]; 
+
+	/* Copy the remaining elements of right subarray 
+	(if there are any) to temp*/
+	while (j <= right) 
+		temp[k++] = arr[j++]; 
+
+	/*Copy back the merged elements to original array*/
+	for (i=left; i <= right; i++) 
+		arr[i] = temp[i]; 
+
+	return inv_count; 
+} 
+
+/* An auxiliary recursive function that sorts the input 
+array and returns the number of inversions in the 
+array. */
+ll int _mergeSort(ll int arr[], ll int temp[], int left, int right) 
+{ 
+	int mid;
+    ll int inv_count = 0; 
+	if (right > left) 
+	{ 
+		/* Divide the array into two parts and call 
+		_mergeSortAndCountInv() for each of the parts */
+		mid = (right + left)/2; 
+
+		/* Inversion count will be sum of inversions in 
+		left-part, right-part and number of inversions 
+		in merging */
+		inv_count = _mergeSort(arr, temp, left, mid); 
+		inv_count += _mergeSort(arr, temp, mid+1, right); 
+
+		/*Merge the two parts*/
+		inv_count += merge(arr, temp, left, mid+1, right); 
+	} 
+
+	return inv_count; 
+} 
+
+/* This function sorts the input array and returns the 
+number of inversions in the array */
+ll int countSwaps(ll int arr[], int n) 
+{ 
+	ll int temp[n]; 
+	return _mergeSort(arr, temp, 0, n - 1); 
+} 
+
+int tc=1;
+void solve()
+    { 
+        int n;
+        cin>>n;
+        ll int arr[n];
+        fo(i,n) cin>>arr[i];
+        // ll int ans=countSwaps(arr,n);
+        // ll int ans1=(n*(n-1))/2-1;
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]<=arr[i+1])
+            {
+                cout<<"YES\n";
+                return;
+            }
+        }
+        
+        {
+            cout<<"NO\n";
+        }
+    }
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    wi(t)
+    {
+      solve();
+    }
+
+    return 0;
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+void ipgraph(int n, int m){
+    int i, u, v;
+    while(m--){
+        cin>>u>>v;
+    u--, v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+}
+
+void dfs(int u, int par){
+    for(int v:g[u]){
+        if (v == par) continue;
+        dfs(v, u);
+    }
+}
+
+
+
+
+
